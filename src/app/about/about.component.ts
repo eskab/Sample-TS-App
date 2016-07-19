caca1e643ec9a6cd27defed6a23bb260567a8de9
almostninja <szymon.kabelis@yahoo.com>
2016-07-19 22:28:44 +0200

    Data binding test

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  public foo: string = 'bar';
 
   constructor() {
     // Do stuff
