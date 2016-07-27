a443de00886117ce57b263e52a8149fdf95a3d28
almostninja <szymon.kabelis@yahoo.com>
2016-07-27 20:52:45 +0200

    Testing part 2

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -21,4 +21,43 @@ describe('HomeService', () => {
     });
   }));
 
+  it('handleError() should log error.message to console', inject([HomeService], s => {
+    spyOn(console, 'log');
+    expect(console.log).not.toHaveBeenCalled();
+
+    const error = {
+      message: 'Strange error occures'
+    };
+
+    s.handleError(error).then(function() {}, function(reason) {
+      expect(reason).toBe(error.message);
+    });
+    expect(console.log).toHaveBeenCalledWith(error.message);
+  }));
+
+  it('handleError() should log error.status and error.statusText to console', inject([HomeService], s => {
+    spyOn(console, 'log');
+    expect(console.log).not.toHaveBeenCalled();
+
+    const error = {
+      status: 404,
+      statusText: 'Not found'
+    };
+
+    s.handleError(error).then(function() {}, function(reason) {
+      expect(reason).toBe(error.status + ' - ' + error.statusText);
+    });
+    expect(console.log).toHaveBeenCalledWith(error.status + ' - ' + error.statusText);
+  }));
+
+  it('handleError() should log "Server error"', inject([HomeService], s => {
+    spyOn(console, 'log');
+    expect(console.log).not.toHaveBeenCalled();
+
+    s.handleError({}).then(function() {}, function(reason) {
+      expect(reason).toBe('Server error');
+    });
+    expect(console.log).toHaveBeenCalledWith('Server error');
+  }));
+
 });
